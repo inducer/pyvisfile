e085cd716a488ac519c25f08636032da88e98b1e
Andreas Kloeckner <inform@tiker.net>
2010-11-04 03:03:24 -0400

    Add support for writing structured Vtk meshes.

---
+++ examples/vtk-structured.py
@@ -0,0 +1,18 @@
+from pyvisfile.vtk import write_structured_grid
+
+import numpy as np
+
+eps = 1e-5
+angle_mesh = np.mgrid[1:2:10j, 0:2*np.pi:20j, 0:np.pi:30j]
+
+r = angle_mesh[0,np.newaxis]
+phi = angle_mesh[1,np.newaxis]
+theta = angle_mesh[2,np.newaxis]
+mesh = np.vstack((
+    r*np.sin(theta)*np.cos(phi),
+    r*np.sin(theta)*np.sin(phi),
+    r*np.cos(theta),
+    ))
+
+write_structured_grid("yo.vts", mesh, 
+        point_data=[("phi", phi)])
