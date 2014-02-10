f64e02a5c1dddec5636dbdd846cbfb36c96359a6
Andreas Kloeckner <inform@tiker.net>
2014-02-10 16:25:29 -0600

    Add Luke's plain 2D example

---
+++ examples/vtk-structured-2d-plain.py
@@ -0,0 +1,14 @@
+# contributed by Luke Olson
+
+import numpy as np
+
+n = 50
+x, y = np.meshgrid(np.linspace(-1, 1, n),
+np.linspace(-1, 1, n))
+
+u = np.exp(-50 * (x**2 + y**2))
+
+from pyvisfile.vtk import write_structured_grid
+mesh = np.rollaxis(np.dstack((x, y)), 2)
+write_structured_grid('test.vts', mesh,
+        point_data=[('u', u[np.newaxis, :, :])])
