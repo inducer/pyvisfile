e7924d7857fd08b4b988ac02429f74fe5b54d5d1
Andreas Klöckner <inform@tiker.net>
2021-05-22 18:21:29 -0500

    Drop autodoc_typehints from sphinx config
    https://github.com/inducer/modepy/pull/38#issuecomment-846472970

--- doc/conf.py
+++ doc/conf.py
@@ -67,9 +67,6 @@ pygments_style = "sphinx"
 
 html_theme = "furo"
 
-html_theme_options = {
-        }
-
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
@@ -138,4 +135,3 @@ intersphinx_mapping = {
 }
 
 autoclass_content = "class"
-autodoc_typehints = "description"
