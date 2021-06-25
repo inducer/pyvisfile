f71e476865213f4bbd5c0521ba8b399c338a4644
Andreas Kloeckner <inform@tiker.net>
2021-06-25 13:51:08 -0500

    Use downloaded shared sphinxconfig

--- doc/conf.py
+++ doc/conf.py
@@ -1,46 +1,12 @@
-# PyVisfile documentation build configuration file, created by
-# sphinx-quickstart on Tue Apr 22 23:46:39 2008.
-#
-# This file is execfile()d with the current directory set to its containing dir.
-#
-# The contents of this file are pickled, so don't put values in the namespace
-# that aren't pickleable (module imports are okay, they're removed automatically).
-#
-# All configuration values have a default value; values that are commented out
-# serve to show the default value.
+from urllib.request import urlopen
 
-# If your extensions are in another directory, add it here.
-# sys.path.append("some/directory")
+_conf_url = \
+        "https://raw.githubusercontent.com/inducer/sphinxconfig/main/sphinxconfig.py"
+with urlopen(_conf_url) as _inf:
+    exec(compile(_inf.read(), _conf_url, "exec"), globals())
 
-# General configuration
-# ---------------------
+copyright = "2010-21, Andreas Kloeckner"
 
-# Add any Sphinx extension module names here, as strings. They can be extensions
-# coming with Sphinx (named "sphinx.ext.*") or your custom ones.
-extensions = [
-        "sphinx.ext.autodoc",
-        "sphinx.ext.intersphinx",
-        "sphinx.ext.mathjax",
-        "sphinx_copybutton",
-        ]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# The suffix of source filenames.
-source_suffix = ".rst"
-
-# The master toctree document.
-master_doc = "index"
-
-# General substitutions.
-project = "PyVisfile"
-copyright = "2010, Andreas Kloeckner"
-
-# The default replacements for |version| and |release|, also used in various
-# other places throughout the built documents.
-#
-# The short X.Y version.
 ver_dic = {}
 with open("../pyvisfile/__init__.py") as vpy_file:
     version_py = vpy_file.read()
@@ -50,86 +16,8 @@ version = ".".join(str(x) for x in ver_dic["VERSION"])
 # The full version, including alpha/beta/rc tags.
 release = version
 
-# There are two options for replacing |today|: either, you set today to some
-# non-false value, then it is used:
-# today = ""
-# Else, today_fmt is used as the format for a strftime call.
-today_fmt = "%B %d, %Y"
-
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
-
-
-# Options for HTML output
-# -----------------------
-
-html_theme = "furo"
-
-# The style sheet to use for HTML and HTML Help pages. A file of that name
-# must exist either in Sphinx' static/ path, or in one of the custom paths
-# given in html_static_path.
-# html_style = "default.css"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
-
-# If not "", a "Last updated on:" timestamp is inserted at every page bottom,
-# using the given strftime format.
-html_last_updated_fmt = "%b %d, %Y"
-
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-# html_use_smartypants = True
-
-# Content template for the index page.
-# html_index = ""
-
-# Custom sidebar templates, maps document names to template names.
-# html_sidebars = {}
-
-# Additional templates that should be rendered to pages, maps page names to
-# template names.
-# html_additional_pages = {}
-
-# If false, no module index is generated.
-# html_use_modindex = True
-
-# If true, the reST sources are included in the HTML build as _sources/<name>.
-# html_copy_source = True
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = "PyVisfiledoc"
-
-# Options for LaTeX output
-# ------------------------
-
-# The paper size ("letter" or "a4").
-# latex_paper_size = "letter"
-
-# The font size ("10pt", "11pt" or "12pt").
-# latex_font_size = "10pt"
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title, author, document class [howto/manual]).
-latex_documents = [("index", "pyvisfile.tex",
-    "PyVisfile documentation", "Andreas Kloeckner",
-    "manual")]
-
-# Additional stuff for the LaTeX preamble.
-# latex_preamble = ""
-
-# Documents to append as an appendix to all manuals.
-# latex_appendices = []
-
-# If false, no module index is generated.
-# latex_use_modindex = True
-
 intersphinx_mapping = {
     "https://docs.python.org/3/": None,
     "https://numpy.org/doc/stable/": None,
     "https://documen.tician.de/modepy": None,
 }
-
-autoclass_content = "class"
