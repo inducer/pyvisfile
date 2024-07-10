a8bf70a2f09b2a064afac9421bf4a305e70dad32
Alexandru Fikl <alexfikl@gmail.com>
2024-07-10 14:35:08 +0300

    examples: move read-qmesh.py into quadmesh.py

--- examples/quadmesh.py
+++ examples/quadmesh.py
@@ -9,6 +9,8 @@ except ImportError as exc:
     raise SystemExit(0) from None
 
 
+# {{{ write mesh
+
 f = silo.SiloFile("qmesh.silo", mode=silo.DB_CLOBBER)
 coord = [
         numpy.linspace(-1.0, 1.0, 50),
@@ -23,3 +25,20 @@ f.put_quadvar1("value", "meshxy", numpy.asarray(value, order="F"), value.shape,
         centering=silo.DB_NODECENT)
 
 f.close()
+
+# }}}
+
+
+# {{{ read mesh
+
+db = silo.SiloFile("qmesh.silo", create=False, mode=silo.DB_READ)
+print(db.get_toc().qmesh_names)
+print(db.get_toc().qvar_names)
+
+qmesh = db.get_quadmesh("meshxy")
+print(qmesh.coords)
+
+qvar = db.get_quadvar("value")
+print(qvar.vals)
+
+# }}}
