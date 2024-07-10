dbe2c6f0d9a27d9689b132a66768a8d9a3e33eaf
Alexandru Fikl <alexfikl@gmail.com>
2024-07-10 09:57:11 +0300

    pyproject: move project metadata and tool configuration

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ class PyUblasExtension(NumpyExtension):
 
 
 def get_config_schema():
-    from aksetup_helper import (ConfigSchema,
-            IncludeDir, LibraryDir, Libraries,
-            Switch, StringListOption, make_boost_base_options)
+    from aksetup_helper import (
+        ConfigSchema, IncludeDir, Libraries, LibraryDir, StringListOption, Switch,
+        make_boost_base_options)
 
     return ConfigSchema(make_boost_base_options() + [
         Switch("USE_SILO", False, "Compile libsilo interface"),
@@ -31,10 +31,9 @@ def get_config_schema():
 
 
 def main():
-    from setuptools import find_packages
-    from aksetup_helper import (hack_distutils, get_config, setup,
-            ExtensionUsingNumpy, check_pybind11, PybindBuildExtCommand,
-            get_pybind_include)
+    from aksetup_helper import (
+        ExtensionUsingNumpy, PybindBuildExtCommand, check_pybind11, get_config,
+        get_pybind_include, hack_distutils, setup)
 
     hack_distutils()
     conf = get_config(get_config_schema(),
@@ -45,14 +44,7 @@ def main():
     extra_library_dirs = []
     extra_libraries = []
 
-    ver_dic = {}
-    ver_file_name = "pyvisfile/__init__.py"
-    with open(ver_file_name) as inf:
-        exec(compile(inf.read(), ver_file_name, "exec"), ver_dic)
-
-    requirements = []
     ext_modules = []
-
     if conf["USE_SILO"]:
         check_pybind11()
 
@@ -70,49 +62,11 @@ def main():
             define_macros=list(extra_defines.items()),
             language="c++",
             ))
-        requirements.append("pybind11>=2.5.0")
 
-    setup(name="pyvisfile",
-            version=ver_dic["VERSION_TEXT"],
-            description="Large-scale Visualization Data Storage",
-            long_description=open("README.rst").read(),
-            classifiers=[
-                "Development Status :: 4 - Beta",
-                "Intended Audience :: Developers",
-                "Intended Audience :: Other Audience",
-                "Intended Audience :: Science/Research",
-                "License :: OSI Approved :: MIT License",
-                "Natural Language :: English",
-                "Programming Language :: C++",
-                "Programming Language :: Python",
-                "Programming Language :: Python :: 3",
-                "Topic :: Multimedia :: Graphics :: 3D Modeling",
-                "Topic :: Scientific/Engineering",
-                "Topic :: Scientific/Engineering :: Mathematics",
-                "Topic :: Scientific/Engineering :: Physics",
-                "Topic :: Scientific/Engineering :: Visualization",
-                "Topic :: Software Development :: Libraries",
-                ],
-
-            author="Andreas Kloeckner",
-            author_email="inform@tiker.net",
-            license="MIT",
-            url="http://mathema.tician.de/software/pyvisfile",
-
-            # dependencies
-            setup_requires=requirements,
-            python_requires="~=3.8",
-            install_requires=[
-                    "pytools>=2013.2",
-                    ] + requirements,
-
-            packages=find_packages(),
-            ext_package="pyvisfile.silo",
-            ext_modules=ext_modules,
-            package_data={"pyvisfile": ["py.typed"]},
-
-            cmdclass={"build_ext": PybindBuildExtCommand},
-            zip_safe=False)
+    setup(
+        ext_modules=ext_modules,
+        cmdclass={"build_ext": PybindBuildExtCommand}
+    )
 
 
 if __name__ == "__main__":
