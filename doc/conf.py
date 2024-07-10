4ba9438bb53946c25f8f1f15d66abb7ff7cad944
Alexandru Fikl <alexfikl@gmail.com>
2024-07-10 10:01:56 +0300

    version: use importlib

--- doc/conf.py
+++ doc/conf.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from importlib import metadata
 from urllib.request import urlopen
 
 _conf_url = \
@@ -5,19 +7,13 @@ _conf_url = \
 with urlopen(_conf_url) as _inf:
     exec(compile(_inf.read(), _conf_url, "exec"), globals())
 
-copyright = "2010-21, Andreas Kloeckner"
-
-ver_dic = {}
-with open("../pyvisfile/__init__.py") as vpy_file:
-    version_py = vpy_file.read()
-
-exec(compile(version_py, "../pyvisfile/__init__.py", "exec"), ver_dic)
-version = ".".join(str(x) for x in ver_dic["VERSION"])
-# The full version, including alpha/beta/rc tags.
-release = version
+copyright = f"2010 - {datetime.today().year}, Andreas Kloeckner"
+release = metadata.version("pyvisfile")
+version = ".".join(release.split(".")[:2])
 
 intersphinx_mapping = {
-        "python": ("https://docs.python.org/3/", None),
-        "numpy": ("https://numpy.org/doc/stable/", None),
         "modepy": ("https://documen.tician.de/modepy", None),
+        "numpy": ("https://numpy.org/doc/stable/", None),
+        "python": ("https://docs.python.org/3/", None),
+        "pytools": ("https://documen.tician.de/pytools", None),
 }
--- pyvisfile/__init__.py
+++ pyvisfile/__init__.py
@@ -1,5 +1,15 @@
-VERSION = (2022, 1, 1)
-VERSION_STATUS = ""
-VERSION_TEXT = ".".join(str(x) for x in VERSION) + VERSION_STATUS
+from importlib import metadata
+from typing import Tuple
 
-__version__ = VERSION_TEXT
+
+def _parse_version(version: str) -> Tuple[Tuple[int, ...], str]:
+    import re
+
+    m = re.match("^([0-9.]+)([a-z0-9]*?)$", VERSION_TEXT)
+    assert m is not None
+
+    return tuple([int(nr) for nr in m.group(1).split(".")]), m.group(2)
+
+
+VERSION_TEXT = __version__ = metadata.version("pyvisfile")
+VERSION, VERSION_STATUS = _parse_version(__version__)
