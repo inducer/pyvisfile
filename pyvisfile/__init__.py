b14bbb6bd871c7663c0c72860738db1dd66adf4d
Andreas Kloeckner <inform@tiker.net>
2024-08-11 14:12:57 +0200

    Fix ruff C409

--- pyvisfile/__init__.py
+++ pyvisfile/__init__.py
@@ -8,7 +8,7 @@ def _parse_version(version: str) -> Tuple[Tuple[int, ...], str]:
     m = re.match("^([0-9.]+)([a-z0-9]*?)$", VERSION_TEXT)
     assert m is not None
 
-    return tuple([int(nr) for nr in m.group(1).split(".")]), m.group(2)
+    return tuple(int(nr) for nr in m.group(1).split(".")), m.group(2)
 
 
 VERSION_TEXT = __version__ = metadata.version("pyvisfile")
