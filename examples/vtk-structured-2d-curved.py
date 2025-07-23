e1b79cd9e3808cf3b8331aead08644f39627f0fa
Alexandru Fikl <alexfikl@gmail.com>
2025-07-23 16:01:41 +0300

    feat: port away from deprecated make_obj_array

--- examples/vtk-structured-2d-curved.py
+++ examples/vtk-structured-2d-curved.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import numpy as np
 
+from pytools import obj_array
+
 from pyvisfile.vtk import write_structured_grid
 
 
@@ -9,18 +11,10 @@ angle_mesh = np.mgrid[1:2:10j, 0:2*np.pi:20j]
 
 r = angle_mesh[0, np.newaxis]
 phi = angle_mesh[1, np.newaxis]
-mesh = np.vstack((
-    r*np.cos(phi),
-    r*np.sin(phi),
-    ))
+mesh = np.vstack([r*np.cos(phi), r*np.sin(phi)])
 
-from pytools.obj_array import make_obj_array
-
-
-vec = make_obj_array([
-    np.cos(phi),
-    np.sin(phi),
-    ])
-
-write_structured_grid("yo-2d.vts", mesh,
-        point_data=[("phi", phi), ("vec", vec)])
+vec = obj_array.new_1d([np.cos(phi), np.sin(phi)])
+write_structured_grid(
+    "yo-2d.vts",
+    mesh,
+    point_data=[("phi", phi), ("vec", vec)])
